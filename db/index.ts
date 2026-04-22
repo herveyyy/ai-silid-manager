66093752c5b436df073d4f950a2a72eb602586a3
Elman Torres <63641794+wela-elman@users.noreply.github.com>
2026-04-22 16:41:10 +0800

    [refactor] change MySQL import method in database index file
    - Updated the MySQL import in the database index file to use `require` instead of `import`, ensuring compatibility with TypeScript ESLint rules.

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool(process.env.DATABASE_URL!);
 
